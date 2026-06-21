2f4a36d3d77f7afc82531d3a732487b86f8ad27f
Anaam Agnihotri <anaamagnihotri1623@gmail.com>
2026-06-21 16:27:19 +0530

    Add waste input form and result route, ignore venv folder

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to ReSource!"
+    return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    waste_item = request.form['waste_item']
+    return f"You entered: {waste_item}"
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
